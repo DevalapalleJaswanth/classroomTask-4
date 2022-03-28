1c74109e58c56a1fbb4571a3d111181ff0b043d5
DevalapalleJaswanth <jaswanthreddy040@gmail.com>
2022-03-28 23:42:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-v5xam7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-v5xam7)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+import ControlledForm from './ControlledForms';
+import ControlledfuncForm from './ControlledfuncForms';
+export default function App() {
+  return (
+    <div>
+      {/* {<ControlledForm />} */}
+      <ControlledfuncForm />
+    </div>
+  );
+}
